376e19373402dada9c51223f08089b38e95e99c9
adriennecordell <adrienne.cordellac@gmail.com>
2023-01-23 15:29:52 -0600

    thougth model

---
+++ models/thought.js
@@ -0,0 +1,60 @@
+const { Schema, model, Types } = require("mongoose")
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId,
+        },
+
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 350,
+        },
+
+        username: {
+            type: String,
+            required: true,
+        }
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+
+)
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String, 
+            required: true,
+            maxLength: 300,
+        },
+
+        username: {
+            type: String,
+            required: true,
+        },
+
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+)
+
+ThoughtSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length
+})
+
+const Thought = model("Thought", ThoughtSchema)
+
+module.exports = Thought
